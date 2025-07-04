98f29b14b517426c78f8cfbc8be7dad3f48917c7
abkaaar <abdullahabubakar461@gmail.com>
2025-07-04 21:08:36 +0100

    feat: implement authentication flow with SignIn component and NextAuth integration

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,14 @@
+import { SignInForm } from '@/components/signin-form'
+import React from 'react'
+
+function SignIn() {
+  return (
+   <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignInForm />
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Button>Hello App</Button>
+        <div className="flex gap-4 items-center flex-col">
+          <h1 className="text-3xl font-bold">Welcome to Boilerplate App</h1>
+          <p className="text-muted-foreground text-sm">
+            This is a simple boilerplate app using Next.js, Prisma, and Tailwind CSS.
+          </p>
+         <Link href="/signin">
+            <Button className="cursor-pointer">Sign In</Button>
+          </Link>
+          
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
