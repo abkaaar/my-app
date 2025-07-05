4fa8eda90596138af10920a297540ebc9d17a1cf
abkaaar <abdullahabubakar461@gmail.com>
2025-07-05 06:49:40 +0100

    feat: implement user authentication with Prisma and NextAuth, add User, Account, Session, and VerificationToken models

--- components/signin-form.tsx
+++ components/signin-form.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { signIn } from "@/auth"
 
 export function SignInForm({
   className,
@@ -24,7 +25,13 @@ export function SignInForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form
+          action={async(formData) => {
+            "use server"
+          const email = formData.get("email") as string
+          await signIn("resend", { email })
+          }}
+          >
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
@@ -35,30 +42,13 @@ export function SignInForm({
                   required
                 />
               </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
               <div className="flex flex-col gap-3">
                 <Button type="submit" className="w-full">
                   Login
                 </Button>
               </div>
             </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
+       
           </form>
         </CardContent>
       </Card>
